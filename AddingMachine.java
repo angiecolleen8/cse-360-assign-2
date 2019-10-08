8c8f3cdd7dcfd5dc26bbb18960ee33af28e6e220
Angela Foley <angelacollen8@gmail.com>
2019-10-08 15:14:58 -0700

    Includes test

--- AddingMachine.java
+++ AddingMachine.java
@@ -11,53 +11,86 @@ package cse360assign2;
  */
 public class AddingMachine {
 
-	private int total;
-	private String transactionHistory;
+    private int total;
+    private String transactionHistory;
 
-	/***
-	 * Constructor
-	 */
-	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
-		transactionHistory = "0";
-	}
+    /***
+     * Constructor
+     */
+    public AddingMachine() {
+        total = 0;  // not needed - included for clarity
+        transactionHistory = "0";
+    }
 
-	/***
-	 * @return the current total
-	 */
-	public int getTotal () {
-		return total;
-	}
+    /***
+     * @return the current total
+     */
+    public int getTotal() {
+        return total;
+    }
 
-	/***
-	 * @param value is added to the total variable
-	 */
-	public void add (int value) {
-		total += value;
-		transactionHistory = transactionHistory + " + " value;
-	}
+    /***
+     * @param value is added to the total variable
+     */
+    public void add(int value) {
+        total += value;
+        transactionHistory = transactionHistory + " + " + value;
+    }
 
-	/***
-	 * @param value is subtracted from the total variable
-	 */
-	public void subtract (int value) {
-		total -= value;
-		transactionHistory = transactionHistory + " - " value;
-	}
+    /***
+     * @param value is subtracted from the total variable
+     */
+    public void subtract(int value) {
+        total -= value;
+        transactionHistory = transactionHistory + " - " + value;
+    }
 
-	/***
-	 * @return a hisotry of the calculation transactions, from the initial 0 value to the last input. Displays the
-	 * normal operators and values
-	 */
-	public String toString () {
-		return "";
-	}
+    /***
+     * @return a hisotry of the calculation transactions, from the initial 0 value to the last input. Displays the
+     * normal operators and values
+     */
+    public String toString() {
+        return transactionHistory;
+    }
 
-	/***
-	 * Clears the current total variable and transaction history
-	 */
-	public void clear() {
-		total = 0;
-		transactionHistory = "0";
-	}
+    /***
+     * Clears the current total variable and transaction history
+     */
+    public void clear() {
+        total = 0;
+        transactionHistory = "0";
+    }
+
+
+    public static void main(String[] args) {
+
+        AddingMachine myCalculator = new AddingMachine();
+        System.out.println("new transactionHistory: " + myCalculator.toString() + "\n");
+        System.out.println("new total = " + myCalculator.total + "\n");
+
+        myCalculator.add(1);
+        System.out.println("//////////////ADD 1//////////////////");
+        System.out.println("new transactionHistory: " + myCalculator.toString() + "\n");
+        System.out.println("new total = " + myCalculator.total + "\n");
+
+        myCalculator.add(10);
+        System.out.println("//////////////ADD 10//////////////////");
+        System.out.println("new transactionHistory: " + myCalculator.toString() + "\n");
+        System.out.println("new total = " + myCalculator.total + "\n");
+
+        myCalculator.subtract(2);
+        System.out.println("//////////////SUB 2//////////////////");
+        System.out.println("new transactionHistory: " + myCalculator.toString() + "\n");
+        System.out.println("new total = " + myCalculator.total + "\n");
+
+        myCalculator.clear();
+        System.out.println("//////////////CLEAR//////////////////");
+        System.out.println("new transactionHistory: " + myCalculator.toString() + "\n");
+        System.out.println("new total = " + myCalculator.total + "\n");
+
+
+    }
 }
+
+
+
