ea86acd7d4993979072506a02afba52a060d2fc3
angiecolleen8 <angiecolleen8@gmail.com>
2019-10-08 12:20:17 -0700

    Initial commit

---
+++ AddingMachine.java
@@ -0,0 +1,58 @@
+/***
+ * Name: Angela Foley
+ * CSE 360, Assignment 2
+ * This file contains class AddingMachine.
+ */
+
+package cse360assign2;
+
+/***
+ * Class AddingMachine has methods that perform various algebraic calculations, and tracks the calculation history.
+ */
+public class AddingMachine {
+
+	private int total;
+
+	/***
+	 * Constructor
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/***
+	 * @return the current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+
+	/***
+	 * @param value is added to the total variable
+	 */
+	public void add (int value) {
+		
+	}
+
+	/***
+	 * @param value is subtracted from the total variable
+	 */
+	public void subtract (int value) {
+		
+	}
+
+	/***
+	 * @return a hisotry of the calculation transactions, from the initial 0 value to the last input. Displays the
+	 * normal operators and values
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/***
+	 * Clears the current total variable
+	 */
+	public void clear() {
+	
+	}
+}
